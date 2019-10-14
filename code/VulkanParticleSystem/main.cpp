c47c2c380bca94412d9ddc7bd6519751a5887655
Robin Wragg <robin.wragg@me.com>
2019-10-14 16:36:42 +0100

    Add GLM and its include path

--- code/VulkanParticleSystem/main.cpp
+++ code/VulkanParticleSystem/main.cpp
@@ -3,6 +3,11 @@
 #include <SDL.h>
 #include <SDL_vulkan.h>
 
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
 using namespace std;
 
 SDL_Renderer *renderer;
