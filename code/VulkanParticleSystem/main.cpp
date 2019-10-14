040afa5d425c64b0e61288081a0a3544ec3e8ff8
Robin Wragg <robin.wragg@me.com>
2019-10-14 18:48:01 +0100

    Vulkan: Select discrete GPU

--- code/VulkanParticleSystem/main.cpp
+++ code/VulkanParticleSystem/main.cpp
@@ -8,7 +8,15 @@
 #include <glm/vec4.hpp>
 #include <glm/mat4x4.hpp>
 
+#include <vulkan/vulkan.h>
+
 using namespace std;
+
+bool enableValidationLayers = true;
+
+vector<const char*> validationLayers = {
+	"VK_LAYER_KHRONOS_validation"
+};
 
 SDL_Renderer *renderer;
 int rendererWidth, rendererHeight;
@@ -18,53 +26,135 @@ double getTime() {
 	return (SDL_GetPerformanceCounter() - startCount) / (double)SDL_GetPerformanceFrequency();
 }
 
-int main(int argc, char* argv[]) {
-	int result = SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_assert(result == 0);
+bool checkValidationLayerSupport() {
+	uint32_t layerCount;
+	vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
 
-	//
-	// create a 4:3 720p window
-	//
-	int windowWidth = 960;
-	int windowHeight = 720;
+	std::vector<VkLayerProperties> availableLayers(layerCount);
+	vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
 
-	SDL_Window *window = SDL_CreateWindow(
-		"Vulkan Particle System", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, windowWidth, windowHeight, SDL_WINDOW_VULKAN);
-	SDL_assert(window != NULL);
+	for (const char* layerName : validationLayers) {
+		bool layerFound = false;
 
-	//
-	// Get Vulkan instance extensions
-	//
+		for (const auto& layerProperties : availableLayers) {
+			if (strcmp(layerName, layerProperties.layerName) == 0) {
+				layerFound = true;
+				break;
+			}
+		}
+
+		if (!layerFound) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+void createVkInstance(const char* appName, SDL_Window *window, VkInstance *instanceOut) {
+	SDL_assert(!enableValidationLayers || checkValidationLayerSupport());
+
+	// App info. TODO: Much of this appInfo may be inessential
+	VkApplicationInfo appInfo = {};
+	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	appInfo.pApplicationName = appName;
+	appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+	appInfo.apiVersion = VK_API_VERSION_1_1;
+
+	// VK instance creation info
+	VkInstanceCreateInfo createInfo = {};
+	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	createInfo.pApplicationInfo = &appInfo;
+
 	unsigned int extensionCount;
 	SDL_Vulkan_GetInstanceExtensions(window, &extensionCount, nullptr);
 
 	vector<const char*> extensionNames(extensionCount);
 	SDL_Vulkan_GetInstanceExtensions(window, &extensionCount, extensionNames.data());
 
-	for (auto &name : extensionNames) {
-		printf("VK extension: %s\n", name);
+	createInfo.enabledExtensionCount = extensionCount;
+	createInfo.ppEnabledExtensionNames = extensionNames.data();
+
+	if (enableValidationLayers) {
+		createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+		createInfo.ppEnabledLayerNames = validationLayers.data();
+	} else createInfo.enabledLayerCount = 0;
+
+	VkResult instanceCreationResult = vkCreateInstance(&createInfo, nullptr, instanceOut);
+	SDL_assert(instanceCreationResult == VK_SUCCESS);
+
+	// Query available Vulkan extensions
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+	vector<VkExtensionProperties> extensions(extensionCount);
+
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+	for (const auto& extension : extensions) {
+		printf("Available extension: %s\n", extension.extensionName);
 	}
+}
 
+void getVkPhysicalDevice(VkInstance instance, VkPhysicalDevice *deviceOut) {
+	*deviceOut = VK_NULL_HANDLE;
 
-	//
-	// create renderer
-	//
+	uint32_t deviceCount = 0;
+	vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+	SDL_assert(deviceCount > 0);
 
-	// Query renderers
-	for (int i = 0; i < SDL_GetNumRenderDrivers(); i++) {
-		SDL_RendererInfo rendererInfo;
-		SDL_GetRenderDriverInfo(i, &rendererInfo);
-		printf("%s\n", rendererInfo.name);
+	std::vector<VkPhysicalDevice> devices(deviceCount);
+	vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+	// This picks any device that supports Vulkan 1.1 (this resolves to the GTX 1060 3GB on my testing hardware).
+	for (auto &device : devices) {
+		VkPhysicalDeviceProperties properties;
+		vkGetPhysicalDeviceProperties(device, &properties);
+
+		if (VK_VERSION_MAJOR(properties.apiVersion) >= 1
+			&& VK_VERSION_MINOR(properties.apiVersion) >= 1) {
+			*deviceOut = device;
+		}
 	}
+}
 
-	Uint32 renderer_flags = SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED;
-	renderer = SDL_CreateRenderer(window, 0, renderer_flags);
-	SDL_assert(renderer != NULL);
-	SDL_RendererInfo rendererInfo;
-	SDL_GetRendererInfo(renderer, &rendererInfo);
-	SDL_GetRendererOutputSize(renderer, &rendererWidth, &rendererHeight);
-	printf("Renderer: %i x %i, %s\n", rendererWidth, rendererHeight, rendererInfo.name);
+int main(int argc, char* argv[]) {
+	const char *appName = "Vulkan Particle System";
 
+	int result = SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_assert(result == 0);
+
+	// create a 800x600 SDL2 window
+	int windowWidth = 800;
+	int windowHeight = 600;
+
+	SDL_Window *window = SDL_CreateWindow(
+		appName, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, windowWidth, windowHeight, SDL_WINDOW_VULKAN);
+	SDL_assert(window != NULL);
+
+	VkInstance vkInstance;
+	createVkInstance(appName, window, &vkInstance);
+
+	VkPhysicalDevice vkPhysicalDevice;
+	getVkPhysicalDevice(vkInstance, &vkPhysicalDevice);
+
+	// Query queue families
+	uint32_t queueFamilyCount = 0;
+	vkGetPhysicalDeviceQueueFamilyProperties(vkPhysicalDevice, &queueFamilyCount, nullptr);
+	printf("%i queue families available\n", queueFamilyCount);
+	std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+	vkGetPhysicalDeviceQueueFamilyProperties(vkPhysicalDevice, &queueFamilyCount, queueFamilies.data());
+
+	printf("Queue Families:\n");
+	for (auto &family : queueFamilies) {
+		printf("Number of queues: %i. Support: ", family.queueCount);
+
+		if (family.queueFlags & VK_QUEUE_GRAPHICS_BIT) printf("graphics ");
+		if (family.queueFlags & VK_QUEUE_COMPUTE_BIT) printf("compute ");
+		if (family.queueFlags & VK_QUEUE_TRANSFER_BIT) printf("transfer ");
+		if (family.queueFlags & VK_QUEUE_SPARSE_BINDING_BIT) printf("sparse binding ");
+		printf("\n");
+	}
+	
 	bool running = true;
 	while (running) {
 		static double previousTime = 0;
@@ -79,17 +169,10 @@ int main(int argc, char* argv[]) {
 			}
 		}
 
-		// clear
-		SDL_SetRenderDrawColor(renderer, 0x22, 0x22, 0x22, 0xff);
-		SDL_RenderClear(renderer);
-
-		SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0xff);
-
-
-		SDL_RenderPresent(renderer);
+		// TODO: render
 	}
 
-	SDL_DestroyRenderer(renderer);
+	//vkDestroyInstance(vkInstance, nullptr);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 
