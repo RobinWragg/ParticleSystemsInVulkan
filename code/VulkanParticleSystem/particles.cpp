7a12c1694acedf67df44281cc3ebcaeed0eee920
Robin Wragg <robin.wragg@me.com>
2019-11-04 11:38:37 +0000

    Almost complete vectorisation

--- code/VulkanParticleSystem/particles.cpp
+++ code/VulkanParticleSystem/particles.cpp
@@ -2,22 +2,25 @@
 
 namespace particles {
 
-	// Multiples of 10000 are divisible by 16
 	uint32_t particleCount = 500000;
+	uint32_t m256Count;
+	const uint32_t floatsPerM256 = 8;
 
 	Particle * renderableParticles;
-	
-	float * buf;
 
-	float * positionsX;
-	float * positionsY;
-	float * positionsZ;
+	vector<__m256> positionsX;
+	vector<__m256> positionsY;
+	vector<__m256> positionsZ;
 
-	float * brightnesses;
+	// TODO: Brightnesses are only updated on a per-float basis, so just make it a vector of floats?
+	vector<__m256> brightnesses;
 
-	vector<float> velocitiesX;
-	vector<float> velocitiesY;
-	vector<float> velocitiesZ;
+	vector<__m256> velocitiesX;
+	vector<__m256> velocitiesY;
+	vector<__m256> velocitiesZ;
+
+	// Enables indexing into the arrays by particle index. Slow, so don't do it often.
+#define M256s_TO_FLOATS(arrayName) ((float*)arrayName.data())
 
 	vector<thread> updaterThreads;
 	HANDLE updateStartSemaphore = NULL;
@@ -58,36 +61,52 @@ namespace particles {
 		graphics::init(window, bindingDesc, { positionAttribDesc, brightnessAttribDesc });
 
 		renderableParticles = new Particle[particleCount];
-		buf = new float[particleCount];
 
-		// The positions* arrays and the brightnesses array all share one contiguous block of memory,
-		// Necessary for passing to Vulkan efficiently.
-		float * particlesBasePtr = (float*)malloc(sizeof(float) * 4 * particleCount);
-		positionsX = particlesBasePtr;
-		positionsY = &particlesBasePtr[particleCount];
-		positionsZ = &particlesBasePtr[particleCount * 2];
-		brightnesses = &particlesBasePtr[particleCount * 3];
+		uint32_t renderableFloatsPerParticle = 4; // x, y, z, brightness
+		uint32_t totalRenderableFloats = renderableFloatsPerParticle * particleCount;
+		
+		// The number of particles must fit into a multiple of __m256 without overlap,
+		// otherwise more complexity is required in the hot code path.
+		SDL_assert_release(totalRenderableFloats % floatsPerM256 == 0);
+		SDL_assert_release(particleCount % floatsPerM256 == 0);
+		m256Count = particleCount / floatsPerM256;
+			
+		positionsX.resize(m256Count);
+		positionsX.shrink_to_fit();
+		positionsY.resize(m256Count);
+		positionsY.shrink_to_fit();
+		positionsZ.resize(m256Count);
+		positionsZ.shrink_to_fit();
+		brightnesses.resize(m256Count);
+		brightnesses.shrink_to_fit();
 
-		velocitiesX.resize(particleCount);
-		velocitiesY.resize(particleCount);
-		velocitiesZ.resize(particleCount);
-		for (uint32_t i = 0; i < particleCount; i++) {
-			positionsX[i] = 1.1f;
-			positionsY[i] = 0.85f - randf() * 10;
-			positionsZ[i] = 0;
+		velocitiesX.resize(m256Count);
+		velocitiesX.shrink_to_fit();
+		velocitiesY.resize(m256Count);
+		velocitiesY.shrink_to_fit();
+		velocitiesZ.resize(m256Count);
+		velocitiesZ.shrink_to_fit();
 
-			velocitiesX[i] = 0;
-			velocitiesY[i] = 0;
-			velocitiesZ[i] = 0;
-		}
+		// Initial state
+		for (auto &x : positionsX) x = _mm256_set1_ps(1.1f);
+		for (uint32_t i = 0; i < particleCount; i++) M256s_TO_FLOATS(positionsY)[i] = 0.85f - randf() * 10;
+		for (auto &z : positionsZ) z = _mm256_set1_ps(0.0f);
+		for (auto &x : velocitiesX) x = _mm256_set1_ps(0.0f);
+		for (auto &y : velocitiesY) y = _mm256_set1_ps(0.0f);
+		for (auto &z : velocitiesZ) z = _mm256_set1_ps(0.0f);
 
 		const uint32_t threadCount = thread::hardware_concurrency();
 
+		uint32_t rangeSum = 0;
+
 		for (uint32_t i = 0; i < threadCount; i++) {
-			uint32_t rangeStartIndex = (i * particleCount) / threadCount;
-			uint32_t rangeEndIndexExclusive = ((i + 1) * particleCount) / threadCount;
+			uint32_t rangeStartIndex = (i * m256Count) / threadCount; // This will round down
+			uint32_t rangeEndIndexExclusive = ((i + 1) * m256Count) / threadCount; // This will round down
 			updaterThreads.push_back(thread(updaterThread, rangeStartIndex, rangeEndIndexExclusive));
+			rangeSum += rangeEndIndexExclusive - rangeStartIndex;
 		}
+
+		SDL_assert_release(rangeSum == m256Count);
 	}
 
 	const float gravity = 1.0f;
@@ -96,57 +115,63 @@ namespace particles {
 	vec3 respawnPosition = { -0.8, -0.1, 0.95 };
 	float stepSize = 0.0f;
 
-	void respawn(int i) {
-		positionsX[i] = respawnPosition.x;
-		positionsY[i] = respawnPosition.y;
-		positionsZ[i] = respawnPosition.z;
+	void assignFloat(vector<__m256> m256s, uint32_t m256Index, uint8_t floatIndex, float value) {
+		float floats[floatsPerM256];
 
-		brightnesses[i] = randf();
+		_mm256_store_ps(floats, m256s[m256Index]);
+		floats[floatIndex] = respawnPosition.x;
+		positionsX[m256Index] = _mm256_load_ps(floats);
+	}
+
+	void respawn(uint32_t m256Index, uint8_t floatIndex) {
+		positionsX[m256Index] = _mm256_set1_ps(respawnPosition.x);
+		positionsY[m256Index] = _mm256_set1_ps(respawnPosition.y);
+		positionsZ[m256Index] = _mm256_set1_ps(respawnPosition.z);
+		
+		brightnesses[m256Index] = _mm256_set1_ps(randf());
 
 		vec3 baseVelocity = { 0.4, -1, -0.1 };
 		const float velocityRandomnessAmount = 0.3f;
 		vec3 velocityRandomness = { randf()-0.5f, randf()-0.5f, randf()-0.5 };
 		velocityRandomness = normalize(velocityRandomness) * velocityRandomnessAmount * (randf()*0.95f+0.05f);
 
-		velocitiesX[i] = baseVelocity.x + velocityRandomness.x;
-		velocitiesY[i] = baseVelocity.y + velocityRandomness.y;
-		velocitiesZ[i] = baseVelocity.z + velocityRandomness.z;
+		velocitiesX[m256Index] = _mm256_set1_ps(baseVelocity.x + velocityRandomness.x);
+		velocitiesY[m256Index] = _mm256_set1_ps(baseVelocity.y + velocityRandomness.y);
+		velocitiesZ[m256Index] = _mm256_set1_ps(baseVelocity.z + velocityRandomness.z);
 	}
 
 	void updateRange(uint32_t startIndex, uint32_t endIndexExclusive) {
 
-		uint32_t count = endIndexExclusive - startIndex;
-		uint32_t vector_count = count / 8;
+		uint32_t localM256Count = endIndexExclusive - startIndex;
 		__m256 stepSizeVector = _mm256_set1_ps(stepSize);
 		__m256 velocityMultiplierVector = _mm256_set1_ps(1 - stepSize * airResistance);
 		__m256 gravityStepVector = _mm256_set1_ps(gravity * stepSize);
 
-		__m256 *velXVector = (__m256*)&velocitiesX[startIndex];
-		__m256 *posXVector = (__m256*)&positionsX[startIndex];
-		__m256 *velYVector = (__m256*)&velocitiesY[startIndex];
-		__m256 *posYVector = (__m256*)&positionsY[startIndex];
-		__m256 *velZVector = (__m256*)&velocitiesZ[startIndex];
-		__m256 *posZVector = (__m256*)&positionsZ[startIndex];
-
 		// x, y, and z are grouped together here for better cache-friendliness
 
-		for (uint32_t i = 0; i < vector_count; i++) {
-			velXVector[i] = _mm256_mul_ps(velXVector[i], velocityMultiplierVector);
-			posXVector[i] = _mm256_add_ps(posXVector[i], _mm256_mul_ps(velXVector[i], stepSizeVector));
+		for (uint32_t i = 0; i < localM256Count; i++) {
+			velocitiesX[i] = _mm256_mul_ps(velocitiesX[i], velocityMultiplierVector);
+			positionsX[i] = _mm256_add_ps(positionsX[i], _mm256_mul_ps(velocitiesX[i], stepSizeVector));
 		}
 
-		for (uint32_t i = 0; i < vector_count; i++) {
-			velYVector[i] = _mm256_add_ps(_mm256_mul_ps(velYVector[i], velocityMultiplierVector), gravityStepVector);
-			posYVector[i] = _mm256_add_ps(posYVector[i], _mm256_mul_ps(velYVector[i], stepSizeVector));
+		for (uint32_t i = 0; i < localM256Count; i++) {
+			velocitiesY[i] = _mm256_add_ps(_mm256_mul_ps(velocitiesY[i], velocityMultiplierVector), gravityStepVector);
+			positionsY[i] = _mm256_add_ps(positionsY[i], _mm256_mul_ps(velocitiesY[i], stepSizeVector));
 		}
 
-		for (uint32_t i = 0; i < vector_count; i++) {
-			velZVector[i] = _mm256_mul_ps(velZVector[i], velocityMultiplierVector);
-			posZVector[i] = _mm256_add_ps(posZVector[i], _mm256_mul_ps(velZVector[i], stepSizeVector));
+		for (uint32_t i = 0; i < localM256Count; i++) {
+			velocitiesZ[i] = _mm256_mul_ps(velocitiesZ[i], velocityMultiplierVector);
+			positionsZ[i] = _mm256_add_ps(positionsZ[i], _mm256_mul_ps(velocitiesZ[i], stepSizeVector));
 		}
 
-		for (uint32_t i = startIndex; i < endIndexExclusive; i++) {
-			if (positionsY[i] > groundLevel) respawn(i);
+		float *yFloats = M256s_TO_FLOATS(positionsY);
+
+		for (uint32_t p = 0; p < particleCount; p++) {
+			if (yFloats[p] > groundLevel) {
+				uint32_t m256Index = p / floatsPerM256;
+				uint32_t m256FloatIndex = p % floatsPerM256;
+				respawn(m256Index, m256FloatIndex);
+			}
 		}
 	}
 
@@ -182,11 +207,16 @@ namespace particles {
 	void render() {
 
 		// Temporary compatibility code for graphics::render(), while the AVX instruction code is developed.
+		float *xFLoats = M256s_TO_FLOATS(positionsX);
+		float *yFloats = M256s_TO_FLOATS(positionsY);
+		float *zFloats = M256s_TO_FLOATS(positionsZ);
+		float *brightnessFloats = M256s_TO_FLOATS(brightnesses);
+		
 		for (uint32_t i = 0; i < particleCount; i++) {
-			renderableParticles[i].position.x = positionsX[i];
-			renderableParticles[i].position.y = positionsY[i];
-			renderableParticles[i].position.z = positionsZ[i];
-			renderableParticles[i].brightness = brightnesses[i];
+			renderableParticles[i].position.x = xFLoats[i];
+			renderableParticles[i].position.y = yFloats[i];
+			renderableParticles[i].position.z = zFloats[i];
+			renderableParticles[i].brightness = brightnessFloats[i];
 		}
 
 		graphics::render(particleCount, renderableParticles);
