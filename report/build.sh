03fb88cfc1eac5bee173839611ed0751ef871d5a
Robin Wragg <robin.wragg@me.com>
2019-11-08 19:54:15 +0000

    Report: update build script

--- report/build.sh
+++ report/build.sh
@@ -1,1 +1,3 @@
-latexmk -e '$pdflatex=q/pdflatex %O -shell-escape %S/' -pdf
+latexmk -e '$pdflatex=q/pdflatex %O -shell-escape -interaction=nonstopmode %S/' -pdf > /dev/null \
+&& texcount main.tex | grep "Words in text" \
+&& open main.pdf
